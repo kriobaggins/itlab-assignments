6883a30e38205ebf4d66f0964ec9bf6ad581dd78
kriobaggins <meherinfinity@gmail.com>
2021-10-09 22:14:20 +0530

    a5

--- a5/index.js
+++ a5/index.js
@@ -2,11 +2,13 @@ const { MongoClient } = require('mongodb');
 const http = require('http');
 const fs = require('fs');
 
-const client = new MongoClient('mongodb://localhost:27017', { useUnifiedTopology: true })
+const client = new MongoClient('mongodb+srv://smc:passwd%40123@smc-cluster.noub5.mongodb.net/test?authSource=admin&replicaSet=atlas-wwuok8-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true', { useUnifiedTopology: true })
 
 async function getApiHandler(req, res) {
 
-  // NOTE: The approach we used in the class is 
+  // NOTE: The approach practiced in the lab practice sessions 
+  // is old syntax. Following the new documentation, I used this below syntax
+  // 
   try {
     await client.connect();
 
@@ -24,9 +26,9 @@ async function getApiHandler(req, res) {
     const data = await cursor.toArray();
 
 
-    if (req.url == "/") {
-      fs.readFile(__dirname + "/public/index.html" , function (_, data) {
-        res.writeHead(301, {Location: "/index.html"});
+    if (req.url === "/") {
+      fs.readFile(__dirname + "/public" + req.url + "index.html" , function (_, data) {
+        res.writeHead(200, {"Content-Type": "text/html"});
         res.end(data);
       });
     }
@@ -34,6 +36,21 @@ async function getApiHandler(req, res) {
       res.writeHeader(200, {"Content-Type": "application/json"});
       res.write(JSON.stringify(data))
       res.end()
+    }
+    // Below is the regex to match all strings other than 
+    // those ending with .[file extension]
+    // https://regex101.com/r/i3kAHs/1
+    else if (req.url.match(/^\/.*(?<!(\..*))$/)) {
+      fs.readFile(__dirname + "/public" + req.url + "/index.html" , function (err, data) {
+        if (err) {
+          res.writeHead(404);
+          res.end(JSON.stringify(err));
+          return;
+        }
+        console.log(req.url)
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(data);
+      });
     } else {
       fs.readFile(__dirname + "/public" + req.url, function (err,data) {
         if (err) {
@@ -50,5 +67,5 @@ async function getApiHandler(req, res) {
   }
 }
 
+// expose the server on port 7080
 http.createServer((req, res) => getApiHandler(req, res).catch(console.dir)).listen("7080")
-
